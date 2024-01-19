fc0dd55d477d517c9433f62aa9408d457fd15e91
Harry <hbedford@scottlogic.com>
2024-01-19 09:10:37 +0000

    init project

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="container">
+    <h1>WELCOME</h1>
+    <p>Dots and Boxes</p>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  height: 100%;
+  margin: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+</style>
